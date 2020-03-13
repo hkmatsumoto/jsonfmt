8244cdeaa68676af53ef0990be80a39fb9fe71d8
Hirochika Matsumoto <matsujika@gmail.com>
2020-03-13 09:41:57 +0900

    Implement formatter

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let () =
+  match Sys.argv with
+  | [| _; json |] ->
+      Jsonfmt.Util.json_of_string json
+      |> Jsonfmt.Formatter.format |> print_endline
+  | _ -> failwith "expected 1 argument"
---
+++ jsonfmt/util.ml
@@ -0,0 +1,1 @@
+let json_of_string = Yojson.Basic.from_string
